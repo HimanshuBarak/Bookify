9680d1cdf283d886cfa3f62d28121e3576684896
HimanshuBarak <himanshubarak11@gmail.com>
2020-12-04 17:28:04 +0530

    added all the preview functionality and css

--- src/Row.js
+++ src/Row.js
@@ -3,10 +3,13 @@ import axios from 'axios'
 import Book from './Book';
 import './row.css'
 
+
+
 function Row() {
     const base_url="https://www.googleapis.com/books/v1/volumes?q=";
     
     const [search, setsearch] = useState('subject:Fiction')
+    const [example,setexample] =useState('Popular Titles in Fiction')
     const [books,setbooks] = useState([])
     
     useEffect(() => {
@@ -15,26 +18,35 @@ function Row() {
           
            const response = await axios.get(base_url+search)
            setbooks(response.data.items);
-           console.log(response);
+          // console.log(response);
             return response;
         }fetchbookdata();
-       
+      if(search!=='subject:Fiction'){
+          setexample('Results found')
+      }
+      else{
+        setexample('Popular Titles in Fiction')
+      }
     }, [search])
-
+    let handlesubmit=e=>{
+        e.preventDefault()
+       
+    }
     return (
         <div className="container">
         <header >
-        <h1>Books App</h1>
+        <h1 style={{color:"#34626c"}}>Bookify</h1>
         <p> <em>Explore among millions of book titles</em></p>
+        <input type="text" placeholder="Search by title,author" onChange={(e)=>setsearch(e.target.value)}/>
+        <h5 >{example}</h5>
+        <hr/>
       </header>  
-          <form className="searchform"  >
-            <input type="text" placeholder="Search by title,author" onChange={(e)=>setsearch(e.target.value)}/>
-            <button>Search</button>    
-
-         </form>
+         
+           
+           
         <div className="row">
             {books.map(book=>{
-                return  <Book  book={book}/>
+                return  <Book id={book.id}  book={book}/>
                  
            })}
         </div>
